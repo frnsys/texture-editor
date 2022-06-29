5cf37417160f6237366d841f19c8fec3ff49dbd1
Francis Tseng <f+accounts@frnsys.com>
2022-06-29 17:49:06 +0200

    way to run

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
 from app import app
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    try:
+        port = int(sys.argv[1])
+    except IndexError:
+        print('Please specify the port.')
+        sys.exit(1)
+    app.run(port=port, debug=True)
